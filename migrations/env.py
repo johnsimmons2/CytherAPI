b68c9974cc3da8a977b805712f0ebc94eb30eb3e
Jonathan Simmons <jsim247@gmail.com>
2023-09-28 03:21:46 -0500

    c

--- migrations/env.py
+++ migrations/env.py
@@ -29,26 +29,14 @@ target_metadata = db.Model.metadata
 # ... etc.
 
 
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
+import os
+url = os.environ.get('DATABASE_URL')
+if url is None:
+    print('Could not get environment variable for DATABASE_URL. Using default in alembic.ini.')
+    url = config.get_main_option("sqlalchemy.url")
+print(f"Using database url: {url}")
 
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
-    import os
-    url = os.environ.get('DATABASE_URL')
-    if url is None:
-        print('Could not get environment variable for DATABASE_URL. Using default in alembic.ini.')
-        url = config.get_main_option("sqlalchemy.url")
-    print(f"Using database url: {url}")
-
-
+def run_migrations() -> None:
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -59,31 +47,4 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-
-# TODO: Get the same configurations as the main project.
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations()
